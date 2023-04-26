bf9915d6a19b191f4c13475b37950db4fc171a37
Arpit Verma <arpitv970@gmail.com>
2023-04-26 18:33:27 +0530

    documented basics of project

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import {chats} from './data/data.js';
 
 const app = express();
 app.use(cors());
+
+app.use('/api/chat', (req, res) => {
+    res.send(chats);
+});
 
 app.use('/', (req, res) => {
     res.send('Welcome to AI powered Chat Application!!');
