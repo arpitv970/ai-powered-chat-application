577f2b79b7bc6c3077ea6a4a2cc4f0a439266439
Arpit Verma <arpitv970@gmail.com>
2023-06-19 00:05:45 +0530

    updated code

--- frontend/src/components/chats/RightCard.jsx
+++ frontend/src/components/chats/RightCard.jsx
@@ -7,12 +7,12 @@ const RightCard = () => {
     const selectedChat = useSelector((state) => state.selectedChats);
     return (
         <div className='border-[2.5px] border-black rounded-xl h-[100%] w-[75%] ml-1 flex flex-col justify-between items-start px-3 py-2'>
-            {Object.keys(selectedChat).length === 0 ? (
+            {Object.keys(selectedChat)?.length === 0 ? (
                 <div>No User Selected</div>
             ) : (
                 <>
                     <RightCardHeader
-                        personName={selectedChat?.users[1]?.name}
+                    chat={selectedChat}
                     />
                     <ChatArea />
                     <RightCardFooter />
--- frontend/src/components/chats/RightCardHeader.jsx
+++ frontend/src/components/chats/RightCardHeader.jsx
@@ -1,9 +1,39 @@
+import { Avatar, Heading, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
-const RightCardHeader = ({ personName }) => {
+const RightCardHeader = ({ chat }) => {
     const userData = useSelector((state) => state.user);
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+
     console.log('aizen', userData?.user?.name);
-    return <div className='text-3xl'>{personName}</div>;
+
+    return <div onClick={onOpen} className='text-3xl py-2 my-2 ml-0 mr-auto flex justify-start items-center w-[100%] hover:bg-gray-300 transition-all ease-in-out duration-200 cursor-pointer rounded-xl'>
+        <Avatar className='mx-3' name={chat?.users[1]?.name} src={chat?.users[1]?.pic} />
+        {chat?.users[1]?.name}
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Profile</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody className='flex flex-col justify-around items-center mx-auto'>
+                    <Heading className='mt-0'>{chat?.users[1]?.name}</Heading>
+                    <Image
+                        className='m-3'
+                        objectFit='cover'
+                        borderRadius='full'
+                        boxSize='50%'
+                        src={chat?.users[1]?.pic}
+                        alt='Dan Abramov'
+                    />
+                    <p className='m-3'>{chat?.users[1]?.email}</p>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    </div>;
 };
 
 export default RightCardHeader;
