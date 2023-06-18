1e45c2c6a8c10e0361e9b6165ff1331ebd9855b8
Arpit Verma <arpitv970@gmail.com>
2023-06-18 17:36:16 +0530

    fixed right card

--- frontend/src/components/chats/RightCard.jsx
+++ frontend/src/components/chats/RightCard.jsx
@@ -7,8 +7,8 @@ const RightCard = () => {
     const selectedChat = useSelector((state) => state.selectedChats);
     return (
         <div className='border-[2.5px] border-black rounded-xl h-[100%] w-[75%] ml-1 flex flex-col justify-between items-start px-3 py-2'>
-            {!selectedChat ? (
-                <div>asd</div>
+            {Object.keys(selectedChat).length === 0 ? (
+                <div>No User Selected</div>
             ) : (
                 <>
                     <RightCardHeader
