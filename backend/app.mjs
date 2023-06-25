585eb6e05b284e3e7d08c509eb8eea8d7a67ced7
Arpit Verma <arpitv970@gmail.com>
2023-06-25 22:21:13 +0530

    updated cros error

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import { chats } from './data/data.mjs';
 import userRouter from './routes/user-routes.mjs';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import chatRouter from './routes/chat-routes.mjs';
 import messageRouter from './routes/message-routes.mjs';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
@@ -20,15 +20,42 @@ app.use('/api/user', userRouter);
 app.use('/api/chat', chatRouter);
 app.use('/api/message', messageRouter);
 
-mongoose
-    .connect(URI)
-    .then(() => {
-        app.listen(PORT);
-        console.log('\n');
-    })
-    .then(() => {
-        console.log(`Connected to MongoDB Atlas & listening at PORT: ${PORT}`);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+let frontendURL = 'http://localhost:5173';
+
+const io = new Server(
+    mongoose
+        .connect(URI)
+        .then(() => {
+            app.listen(PORT);
+            console.log('\n');
+        })
+        .then(() => {
+            console.log(
+                `Connected to MongoDB Atlas & listening at PORT: ${PORT}`
+            );
+        })
+        .catch((err) => {
+            console.log(err);
+        }),
+    {
+        pingTimeout: 60000,
+        cors: {
+            origin: 'http://localhost:5173',
+        },
+    }
+);
+
+io.on('con');
+
+// mongoose
+//     .connect(URI)
+//     .then(() => {
+//         app.listen(PORT);
+//         console.log('\n');
+//     })
+//     .then(() => {
+//         console.log(`Connected to MongoDB Atlas & listening at PORT: ${PORT}`);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
--- frontend/axios.js
+++ frontend/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const AxiosHelper = axios.create({
     baseURL: 'http://localhost:5000/api/',
+    headers: { 'Access-Control-Allow-Origin': '*' },
 });
 
 export default AxiosHelper;
